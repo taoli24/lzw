2e04a5146af54406bcf888d4ce06ed3777b18390
Li Tao <gujie713@gmail.com>
2023-01-16 23:32:29 +1100

    optimised for binary

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-def lzw_encoder(input_data, bits=9):
+def lzw_encoder(input_data, bits = 6, binary = True):
     table = {}
     res = []
 
-    max_key = sum(2 ** i for i in range(bits))
+    max_key = sum(2**i for i in range(bits))
 
     # creating default compression table - ASCII
-    for i in range(0, 255):
-        table[chr(i + 1)] = i + 1
+    if not binary:
+        for i in range(0, 255):
+            table[chr(i + 1)] = i + 1
+        next = 255
+    else:
+        for i in range(2):
+            table[str(i)] = i
+        next = 2
 
-    next = 255
     idx = 0
-
     while idx < len(input_data):
-
         current = input_data[idx]
         current_idx = idx
 
@@ -30,23 +33,27 @@ def lzw_encoder(input_data, bits=9):
             next += 1
 
         current = current[:-1]
-
         res.append(table[current])
 
-    n_bits = len(bin(len(table.keys())).replace("0b", ""))
-
-    return res, n_bits, {v: k for k, v in table.items()}
+    return res, bits ,{v:k for k,v in table.items()}
 
 
-def lzw_decode(lzw_encoding, bits=9):
+
+def lzw_decode(lzw_encoding, bits = 6, binary=True):
     table = {}
     res = ""
-    next_ind = 255
 
-    max_key = sum(2 ** i for i in range(bits))
+    max_key = sum(2**i for i in range(bits))
 
-    for i in range(0, 255):
-        table[i + 1] = chr(i + 1)
+    # creating default compression table - ASCII
+    if not binary:
+        for i in range(0, 255):
+            table[i + 1] = chr(i + 1)
+        next_ind = 255
+    else:
+        for i in range(2):
+            table[i] = str(i)
+        next_ind = 2
 
     for x in range(len(lzw_encoding)):
         encoding = lzw_encoding[x]
@@ -63,6 +70,7 @@ def lzw_decode(lzw_encoding, bits=9):
                 res += current
 
                 if next_ind <= max_key:
-                    table[next_ind] = current + table.get(lzw_encoding[x + 1], current)[0]
+                    table[next_ind] = current + table.get(lzw_encoding[x+1], current)[0]
                     next_ind += 1
+
     return res
